aab5df91fd7a5b83c99dcc29ac1b7ed9ee17905d
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-01-19 17:38:13 -0800

    Código funcional, antes de ver el video Eliminando productos del carrito (26/30)

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -6,6 +6,14 @@ import flechita from "@icons/flechita.svg";
 
 const MyOrder = () => {
   const { state } = useContext(AppContext);
+
+  const sumTotal = () => {
+    const reducer = (accumalator, currentValue) =>
+      accumalator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -20,7 +28,7 @@ const MyOrder = () => {
           <p>
             <span>Total</span>
           </p>
-          <p>$100.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
